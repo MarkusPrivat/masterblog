79659434e6eea33315a03d01542f7caedb73a955
Markus <25428460+MarkusPrivat@users.noreply.github.com>
2026-03-04 15:56:54 +0100

    added /add.html to create Blog posts

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from data.storage import load_blogs_posts
-from flask import Flask, render_template
+from datetime import datetime
+
+
+from flask import Flask, render_template, request, redirect, url_for
+
+from data.storage import load_blog_posts, append_blog_post
+
+
+
+
 
 
 app = Flask(__name__)
@@ -7,8 +15,37 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    blog_posts = load_blogs_posts()
+    is_executed, blog_posts = load_blog_posts()
+    if not is_executed:
+        blog_posts = {
+            "id": "1",
+            "author": "Admin",
+            "title": "Sorry, something went wrong.",
+            "content": "Blog posts could not be loaded."
+        }
     return render_template('index.html', blog_posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+
+        # TODO: Fill this in the next step
+        author = request.form.get('author', '')
+        title = request.form.get('title', '')
+        content = request.form.get('content', '')
+        blog_post = {
+            'id': str(datetime.now()),
+            'author': author,
+            'title': title,
+            'content': content
+        }
+        is_executed, msg = append_blog_post(blog_post)
+        if not is_executed:
+            return msg
+        return redirect(url_for('home'))
+    return render_template('add.html')
+
 
 
 if __name__ == '__main__':
--- data/storage.py
+++ data/storage.py
@@ -7,16 +7,37 @@ PROJECT_ROOT = Path(__file__).parent
 BLOG_POSTS_JSON_PATH = PROJECT_ROOT / "blog_posts.json"
 
 
-def load_blogs_posts() -> list[dict]:
+def load_blog_posts() -> tuple[bool, list[dict]]:
     try:
         with open(BLOG_POSTS_JSON_PATH, 'r', encoding='utf-8') as fileobj:
-            return json.loads(fileobj.read())
+            return True, json.loads(fileobj.read())
     except (json.JSONDecodeError, FileNotFoundError):
-        return []
+        return False, []
+
+
+def save_blog_posts(blog_posts: list[dict]) -> tuple[bool, str]:
+    try:
+        with open(BLOG_POSTS_JSON_PATH, 'w', encoding='utf-8') as fileobj:
+            json.dump(blog_posts, fileobj, indent=4)
+            return True, "Done"
+    except Exception as error:
+        return False, error
+
+
+
+def append_blog_post(blog_post: dict) -> tuple[bool, str]:
+    is_executed, blog_posts = load_blog_posts()
+    if not is_executed:
+        return False, "Could not load blog_post.json"
+    blog_posts.append(blog_post)
+    is_executed, msg = save_blog_posts(blog_posts)
+    if not is_executed:
+        return False, f"Unexpected error: {msg}"
+    return True, "Done"
 
 
 def main():
-    print(load_blogs_posts())
+    print(load_blog_posts())
 
 
 if __name__ == '__main__':
